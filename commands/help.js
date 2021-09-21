4a643d052fe7c96b234b69df09c1856c585a7da3
nwhloo99 <nicholaswhloo99@gmail.com>
2021-09-21 23:42:03 +0800

    Update help command to include new funcions

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,10 @@ module.exports = {
         helpMessage += "**2** #skip - Skips the currently playing song\n";
         helpMessage += "**3** #stop - Stops playing the song and clears the list\n";
         helpMessage += "**4** #list - Lists all the songs in the playlist\n";
+        helpMessage += "**5** #save_song {song name} {youtube url} - Saves song as **song name** to  database\n";
+        helpMessage += "**6** #play_song {song name} - Plays the song from the database\n";
+        helpMessage += "**7** #yawn - YAWNNNNN\n";
+        helpMessage += "**8** #ambulance - Call an AMBULANCE \n";
         return message.channel.send(helpMessage);
     }
 }
